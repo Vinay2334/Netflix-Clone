045acd5a4b68db1a701a35cb0534196e9ba4f0b5
Vinay <vinaybhardwaj2334@gmail.com>
2022-09-30 14:19:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect } from 'react'
 import HomeScreen from './HomeScreen'
+import ProfileScreen from './ProfileScreen';
 import {
   BrowserRouter as Router,
   Routes,
@@ -8,20 +9,28 @@ import {
 } from "react-router-dom";
 import LoginScreen from './LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout,login, selectUser } from './features/counter/userSlice';
 function App() {
-  const user=null;
+  const user=useSelector(selectUser);
+  const dispatch=useDispatch()
 
   useEffect(() => {
     const unsubscribe=auth.onAuthStateChanged(userAuth => {
       if(userAuth) {
         //Logged in
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
       }else {
         //logged out
+        dispatch(logout())
       }
     })
 
     return unsubscribe;
-  }, [])
+  }, [dispatch])
   
 
   return (
@@ -31,6 +40,7 @@ function App() {
           <LoginScreen/>
         ):(
           <Routes>
+          <Route exact path="/profile"element={<ProfileScreen/>}></Route>
           <Route exact path="/"element={<HomeScreen/>}></Route>
           </Routes>
         )
